3ba97d5a1c20cfaad71aa72a349419ec5501f25b
jessifedalto <jessicafedalto.jf@gmail.com>
2024-09-13 16:48:48 -0300

    feat: started user controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,5 @@
+using EntityFramework.DTO;
+using EntityFramework.Model;
 using EntityFramework.Repositories;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -6,18 +8,41 @@ namespace EntityFramework.Controllers;
 
 [ApiController]
 [Route("user")]
-public class UserController : ControllerBase
+public class UserController(IUserRepository repo) : ControllerBase
 {
-    readonly IUserRepository userRepository;
-    readonly IChannelRepository channelRepository;
-
-    public UserController(IUserRepository repo)
-        => this.userRepository = repo;
-
-    [HttpGet]
+    [HttpGet("/")]
     public async Task<ActionResult> GetChannels(Guid guid)
     {
-        var channels = GetChannels(guid);
+        var channels = await repo.GetChannels(guid);
         return Ok(channels);
     }
+
+    public async Task<ActionResult> GetById(Guid guid)
+    {
+        var user = await repo.GetById(guid);
+
+        if (user is null)
+            return NotFound();
+
+        return Ok(user);
+
+    }
+
+    public async Task<ActionResult> CreateUser(UserDto payload)
+    {
+        var user = new User {
+            Name = payload.Name,
+            UserName = payload.UserName,
+            BirthDate = payload.BirthDate,
+            Email = payload.Email,
+            Password = payload.Password,
+            UserRole = payload.UserRole
+        };
+
+        await repo.Add(user);
+
+        return Created("/user", user);
+    }
+
+    
 }
